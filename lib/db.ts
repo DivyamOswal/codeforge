e520201774e0d15c1937e49d664879831dfe2284
DivyamOswal <divyam9850400550@gmail.com>
2026-08-11 23:31:31 +0530

    feat: refactor database connection to use connection pooling with Prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,22 @@
+import { Pool } from "pg";
 import { PrismaClient } from "./generated/prisma/client";
 import {PrismaPg} from "@prisma/adapter-pg"
 
-export const prisma = new PrismaClient({
-    adapter: new PrismaPg({connectionString: process.env.DATABASE_URL})
+// Caching the db to prevent mutiple connection to the db because of nextjs hot reload
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
 })
+
+const adapter = new PrismaPg(pool)
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+    adapter: adapter
+})
+
+if(process.env.NODE_ENV !=="production"){
+    globalForPrisma.prisma = prisma
+}
